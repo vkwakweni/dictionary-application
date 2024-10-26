094cb057f225d6449499f5d4da6737a68c4476f5
Vuyo Kwakweni <b2fkcj@tr.pte.hu>
2024-10-26 20:34:41 +0200

    added references to SQL tables; left comments for defining the SQL references in the model layer

--- demo/src/main/java/com/groupproject/model/Word.java
+++ demo/src/main/java/com/groupproject/model/Word.java
@@ -1,15 +1,37 @@
 
 package com.groupproject.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.SecondaryTable;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "words")
+@SecondaryTable(name = "synsets")
+@NamedQueries({ // what type of queries would go here?
+    
+})
 public class Word {
-     private String word;
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "lemma")
+    private String word;
+    /*
+     * we don't have definitinos in the same table as words right now, so, the possible
+     * solutions are:
+     * - https://docs.oracle.com/javaee/7/api/javax/persistence/SecondaryTable.html
+     * - https://stackoverflow.com/questions/66982026/jpa-secondary-table-mapping-direction
+     */
+    @Column(name = "definition", table = "synset")
     private String definition;
 
     // Constructors, Getters and Setters
     public Word() {}
 
     public Word(String word, String definition) {
+        // if we make one table, we can maybe make the constructor with just ID
         this.word = word;
         this.definition = definition;
     }
@@ -29,5 +51,5 @@ public class Word {
     public void setDefinition(String definition) {
         this.definition = definition;
     }
-    
+
 }
