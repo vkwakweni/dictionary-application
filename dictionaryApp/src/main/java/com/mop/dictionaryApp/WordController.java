d7c89a2b511d13f8754b59f8a150c1fb96b04ffb
Vuyo Kwakweni <b2fkcj@tr.pte.hu>
2024-11-08 10:11:37 +0100

    changed mapping in the controller class

--- dictionaryApp/src/main/java/com/mop/dictionaryApp/WordController.java
+++ dictionaryApp/src/main/java/com/mop/dictionaryApp/WordController.java
@@ -1,11 +1,18 @@
 package com.mop.dictionaryApp;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
@@ -13,14 +20,26 @@ public class WordController {
     @Autowired
     private WordService wordService;
 
-    @PostMapping(value = "/add")
-    public void addWord(){
-        wordService.addWord();
+    @GetMapping("/words")
+    public List<Word> getAllWords() {
+        return wordService.getAllWords();
     }
 
-    @GetMapping("/view")
-    public List<Word> getWords() {
-        return wordService.getWords();
+    @GetMapping("/{id}")
+    public ResponseEntity<Word> getWordById(@PathVariable Integer id) {
+        Optional<Word> word = wordService.getWordById(id);
+        return word.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Word createWord(@RequestBody Word word) {
+        return wordService.createWord(word);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteWord(@PathVariable Integer id) {
+        wordService.deleteWord(id);
+        return ResponseEntity.noContent().build();
     }
     
     @GetMapping("/welcome")
