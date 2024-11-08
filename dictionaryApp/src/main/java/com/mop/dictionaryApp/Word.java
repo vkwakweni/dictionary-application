d34d7b8fa2f131d1fa6f32eb0e962cadd78b5773
Vuyo Kwakweni <b2fkcj@tr.pte.hu>
2024-11-08 10:11:06 +0100

    changed model class to include SQL annotations

--- dictionaryApp/src/main/java/com/mop/dictionaryApp/Word.java
+++ dictionaryApp/src/main/java/com/mop/dictionaryApp/Word.java
@@ -1,12 +1,33 @@
 package com.mop.dictionaryApp;
 
-import java.util.List;
+import java.io.Serializable;
 
-public class Word {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.SecondaryTable;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "senses")
+@SecondaryTable(name = "synsets", pkJoinColumns = @PrimaryKeyJoinColumn(name = "synsetid"))
+@SecondaryTable(name = "words", pkJoinColumns = @PrimaryKeyJoinColumn(name = "wordid"))
+public class Word implements Serializable {
+    @Id
+    @Column(name = "wordid")
     private Integer wordID;
+    @Column(name = "synsetid")
     private Integer definitionID;
+    @Column(name = "lemma")
     private String word;
-    private List<Word> synonyms;
+
+    public Word() {}
+
+    public Word(Integer wordId) {
+        this.wordID = wordId;
+        // and then SQL calls to fill in the rest.
+    }
 
     public Integer getWordId(){
         return wordID;
@@ -24,20 +45,17 @@ public class Word {
         this.definitionID = id;
     }
 
-    public String getWord(){
+    public String getWord(Integer id){
         return word;
     }
-    
+
     public void setWord(String word){
         this.word = word;
     }
 
-    public List<Word> getSynonyms(){
-        return synonyms;
-    }
-    
-    public void setSynonyms(List<Word> synonyms){
-        this.synonyms = synonyms;
+    @Override
+    public String toString() {
+        return wordID + ": " + word;
     }
 
 }
