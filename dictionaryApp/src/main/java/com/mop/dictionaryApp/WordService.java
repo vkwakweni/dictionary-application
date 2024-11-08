79400910814879420177bdb8405845c61ef9fe9c
Vuyo Kwakweni <b2fkcj@tr.pte.hu>
2024-11-08 10:12:02 +0100

    updated service class functions

--- dictionaryApp/src/main/java/com/mop/dictionaryApp/WordService.java
+++ dictionaryApp/src/main/java/com/mop/dictionaryApp/WordService.java
@@ -1,25 +1,39 @@
 package com.mop.dictionaryApp;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WordService {
-    private List<Word> dictionary = new ArrayList<>();
-    private List<Word> notYet = new ArrayList<>();
 
-    public void addWord(){
-        Word love = new Word();
-        love.setWordId(79459);
-        love.setDefinitionId(107558676);
-        love.setWord("love");
-        love.setSynonyms(notYet);
-        dictionary.add(love);
+    private final WordRepository wordRepository;
+
+    @Autowired
+    public WordService(WordRepository wordRepository){
+        this.wordRepository = wordRepository;
     }
 
-    public List<Word> getWords() {
-        return dictionary;
+    public List<Word> getAllWords() {
+        return wordRepository.findAll();
     }
+
+    public Optional<Word> getWordById(Integer id) {
+        return wordRepository.findById(id);
+    }
+
+    public Word createWord(Word word) {
+        return wordRepository.save(word);
+    }
+
+    public void deleteWord(Integer id) {
+        if (wordRepository.existsById(id)) {
+            wordRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Word not found with id: " + id);
+        }
+    }
+
 }
