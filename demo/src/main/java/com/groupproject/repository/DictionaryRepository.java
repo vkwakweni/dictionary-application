ce89402555047a3b7389cf88d87f3300b349aa7a
Vuyo Kwakweni <b2fkcj@tr.pte.hu>
2024-11-14 12:44:57 +0100

    removed previous application attempt

--- demo/src/main/java/com/groupproject/repository/DictionaryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.groupproject.repository;
-
-import com.groupproject.model.Word;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DictionaryRepository extends JpaRepository<Word, Integer> {
-
-    
-}
