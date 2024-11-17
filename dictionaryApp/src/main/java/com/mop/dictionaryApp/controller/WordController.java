34ebb8c3a265624231f4e3369417ab86579f2306
Juan Esteban Medina Delgado <nottherealone@email.com>
2024-11-17 22:09:45 +0100

    Revert "sagger-ui running"
    This reverts commit cccbae85bb95ea5e8da3c6242a48fd34872cf8ef.

--- dictionaryApp/src/main/java/com/mop/dictionaryApp/controller/WordController.java
+++ dictionaryApp/src/main/java/com/mop/dictionaryApp/controller/WordController.java
@@ -16,12 +16,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 
 @RestController
-@Api(value = "Word API", description = "Operations related to words and definitions")
 public class WordController {
     @Autowired
     private WordService wordService;
@@ -29,14 +25,13 @@ public class WordController {
     public WordController(WordService wordService) {
         this.wordService = wordService;
     }
-    @ApiOperation(value = "Fetch a word by ID", response = Word.class)
-    @GetMapping("/{wordid:[0-9]+}")
-    public ResponseEntity<Word> getWordWithDefinitions( 
-        @ApiParam(value = "ID of the word", required = true) @PathVariable Integer wordid){
+
+    @GetMapping("/{wordid}")
+    public ResponseEntity<Word> getWordWithDefinitions(@PathVariable Integer wordid) {
         Optional<Word> word = wordService.getWordWithDefinitionsById(wordid);
         return word.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
-    @ApiOperation(value = "Fetch all words with definitions")
+
     @GetMapping("/words")
     public List<Word> getAllWordsWithDefinitions() {
         return wordService.getAllWordsWithDefinitions();
@@ -65,7 +60,7 @@ public class WordController {
         return wordService.createWord(word);
     }
 
-    @DeleteMapping("/{wordid:[0-9]+}")
+    @DeleteMapping("/{wordId}")
     public ResponseEntity<Void> deleteWord(@PathVariable Integer wordid) {
         wordService.deleteWord(wordid);
         return ResponseEntity.noContent().build();
