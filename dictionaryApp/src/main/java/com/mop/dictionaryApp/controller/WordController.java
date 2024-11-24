dba75a49161c7ed07fa2282acc1f38ad3c4882ea
Vuyo Kwakweni <b2fkcj@tr.pte.hu>
2024-11-24 20:34:13 +0100

    removed unnecessary imports from WordController

--- dictionaryApp/src/main/java/com/mop/dictionaryApp/controller/WordController.java
+++ dictionaryApp/src/main/java/com/mop/dictionaryApp/controller/WordController.java
@@ -8,15 +8,12 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mop.dictionaryApp.model.Word;
 import com.mop.dictionaryApp.service.WordService;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
