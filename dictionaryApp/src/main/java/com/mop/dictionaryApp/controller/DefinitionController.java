a0ce5b715cbd2240245793e1cd1d226a24db657b
Vuyo Kwakweni <b2fkcj@tr.pte.hu>
2024-11-24 20:10:22 +0100

    removed unnecessary getAllDefinitions mapping for DefinitionController

--- dictionaryApp/src/main/java/com/mop/dictionaryApp/controller/DefinitionController.java
+++ dictionaryApp/src/main/java/com/mop/dictionaryApp/controller/DefinitionController.java
@@ -1,12 +1,8 @@
 package com.mop.dictionaryApp.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mop.dictionaryApp.model.Definition;
 import com.mop.dictionaryApp.service.DefinitionService;
 
 @RestController
@@ -17,11 +13,6 @@ public class DefinitionController {
 
     public DefinitionController(DefinitionService definitionService) {
         this.definitionService = definitionService;
-    }
-
-    @GetMapping("/synonyms")
-    public List<Definition> getAllDefinitions() {
-        return definitionService.getAllDefinitions();
     }
     
 }
